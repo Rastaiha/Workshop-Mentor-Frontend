d5375dada4d7c4f348a5f68ddc97daa8b21d7b5c
Seyed Alireza Hashemi <45389673+AmooHashem@users.noreply.github.com>
2022-07-08 00:13:05 +0430

    correct dictation mistake

--- src/components/Appbar/ResponsiveAppBar/MentorDashboardItems.js
+++ src/components/Appbar/ResponsiveAppBar/MentorDashboardItems.js
@@ -8,7 +8,7 @@ import LogoutButton from './components/LogoutButton';
 const DashboardItems = () => {
   const logoButton = <LogoButton />;
   const events = <DashboardButton name={'رویدادها'} to={'/events'} />;
-  const correction = <DashboardButton name={'تصحبح'} to={'/correction'} />;
+  const correction = <DashboardButton name={'تصحیح'} to={'/correction'} />;
   const articles = <DashboardButton name={'مقاله‌ها'} to={'/articles'} />;
   const logoutButton = <LogoutButton />;
   const Avatar = <AvatarComponent />;
